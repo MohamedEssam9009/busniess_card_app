ff2777f8fa275e6e11c098c04254ddac6b4d978b
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-05-27 03:43:52 +0300

    container and spacer widgets

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,93 @@ class BusinessCardApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: const Color(0xFF2B475E),
         body: Column(
-          children: const [
-            CircleAvatar(
-              radius: 102.0,
+          children: [
+            const CircleAvatar(
+              radius: 122.0,
               backgroundColor: Colors.white,
               child: CircleAvatar(
-                radius: 100.0,
+                radius: 120.0,
                 backgroundImage: AssetImage('images/my_image.png'),
+              ),
+            ),
+            const Text(
+              'Mohamed Essam',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32.0,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+            const Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                color: Color(0xFF6C8090),
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(6.0),
+                  color: Colors.white,
+                ),
+                height: 65.0,
+                child: Row(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Icon(
+                        Icons.phone,
+                        size: 32.0,
+                        color: Color(0xFF2B475E),
+                      ),
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Text(
+                      '(+20) 1019664820',
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+                    Spacer(
+                      flex: 3,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(6.0),
+                  color: Colors.white,
+                ),
+                height: 65.0,
+                child: Row(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Icon(
+                        Icons.mail,
+                        size: 32.0,
+                        color: Color(0xFF2B475E),
+                      ),
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Text(
+                      'mohamed.essam900009@gmail.com',
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+                    Spacer(
+                      flex: 3,
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
